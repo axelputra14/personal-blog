5e472116c3ae9064245f48833890f31dd53ba1b8
Octavian Axel Putra <octavian.axel.putra@gmail.com>
2025-04-02 22:55:04 +0700

    update design

--- src/views/VNView.vue
+++ src/views/VNView.vue
@@ -107,11 +107,15 @@ export default {
                   </div>
                   <div class="separator"></div>
                   <div>
+                    <p class="pl-4">Kyou no tenki wa ii ne</p>
+                  </div>
+                  <div class="separator"></div>
+                  <div>
                     <p class="pl-4">今日はいい天気ですね</p>
                   </div>
                   <div class="separator"></div>
                   <div class="title">
-                    <p class="text-white pl-4">Title Title Title</p>
+                    <p class="text-white pl-4">nanti logo</p>
                   </div>
                 </div>
               </section>
